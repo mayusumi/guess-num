e064228f64077c7bd116c7a777ca50cd75e1b582
mayusumi <s0918048981@gmail.com>
2021-02-04 13:16:09 +0800

    basic version

---
+++ guess.py
@@ -0,0 +1,17 @@
+#
+#
+#
+# 猜錯的話 要告訴他 比答案大/小
+import random
+
+r = random.randint(1, 100)
+while True:
+	num = input('請猜數字: ')
+	num = int(num)
+	if num == r:
+		print('你猜中了!')
+		break
+	elif num > r:
+		print('比答案大')
+	elif num < r:
+		print('比答案小')
